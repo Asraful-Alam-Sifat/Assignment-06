19645a3cc16097c9ee10b66c427513d706bda760
Asraful Alam <asrafulalam.origin@gmail.com>
2026-03-30 01:14:07 +0600

    Add NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,53 @@
+
+import { ShoppingCart } from 'lucide-react';
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm px-3 lg:px-10  flex justify-evenly">
+         
+            <div className="navbar-start w-[20%]">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li className='font-semibold text-xl'><a href="/">Home</a></li>
+        <li className='font-semibold text-xl'><a href="/">Features</a></li>
+        <li className='font-semibold text-xl'><a href="/">Pricing</a></li>
+        <li className='font-semibold text-xl'><a href="/">Testimonials</a></li>
+        <li className='font-semibold text-xl'><a href="/">FAQ</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-transparent bg-clip-text bg-linear-to-r from-[#4F39F6] to-[#9514FA] font-bold text-2xl">DigiTools</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+        <li className='font-semibold text-base'><a href="/">Home</a></li>
+        <li className='font-semibold text-base'><a href="/">Features</a></li>
+        <li className='font-semibold text-base'><a href="/">Pricing</a></li>
+        <li className='font-semibold text-base'><a href="/">Testimonials</a></li>
+        <li className='font-semibold text-base'><a href="/">FAQ</a></li>
+    </ul>
+  </div>
+       
+  
+
+  <div className='flex gap-5 items-center'>
+    <ShoppingCart />
+    <button className='text-base font-semibold'>LogIn</button>
+    <div className="navbar-end">
+    <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full">Get Started</a>
+  </div>
+  </div>
+  
+</div>
+        </div>
+    );
+};
+
+export default NavBar;
