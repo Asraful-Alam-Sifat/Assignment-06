67f4f69d233b298ee4c2ab101978ace1ab118511
Asraful Alam <asrafulalam.origin@gmail.com>
2026-03-30 17:45:41 +0600

    add dynamic tool badge to tool card

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -5,12 +5,27 @@ const Tools = ({toolsPromise}) => {
     const toolsData = use(toolsPromise);
     console.log(toolsData);
     return (
-        <div className='max-w-260 mx-auto mt-15 grid grid-cols-1 justify-center md:grid-cols-3 gap-8 md:gap-x-7'>
+        <div 
+        className='max-w-260 mx-auto mt-15 grid grid-cols-1 justify-center md:grid-cols-3 gap-8 md:gap-x-7'>
+
             {toolsData.map(tool => 
-                <div key={tool.id} className='border-2 border-gray-300 rounded-xl p-5 w-full md:max-w-80 mx-auto'>
-                    <div>
-                        <img className='w-9' src={tool.icon} alt={tool.name} />
-                        <div>{tool.tag}</div>
+                <div 
+                key={tool.id} 
+                className='border-2 border-gray-300 rounded-xl p-5 w-full md:max-w-80 mx-auto relative'>
+                    <div className='flex justify-between '>
+                        <img 
+                        className='w-12 p-2 border border-gray-200 rounded-full' 
+                        src={tool.icon} 
+                        alt={tool.name} />
+                        <div className='absolute top-2 right-2 '>
+                            {tool.tagType.toLowerCase() === 'best-seller'
+                            ? <span className='bg-yellow-100 text-yellow-600 px-3 py-0.5 rounded-xl font-medium text-xm'>{tool.tag}</span>
+                            : tool.tagType.toLowerCase() === 'popular'
+                                ? <span className='bg-green-100 text-green-600 px-3 py-0.5 rounded-xl font-medium text-xm'>{tool.tag}</span>
+                                : tool.tagType.toLowerCase() === 'new'
+                                    ? <span className='bg-blue-100 text-blue-600 px-3 py-0.5 rounded-xl font-medium text-xm'>{tool.tag}</span>
+                                    :'' }
+                        </div>
                     </div>
 
                     <h4>{tool.name}</h4>
@@ -19,13 +34,18 @@ const Tools = ({toolsPromise}) => {
                     <h4><span>${tool.price}</span>/{tool.period}</h4>
                     <ul>
                         {tool.features.map((feature, index) => {
-                            return <li key={index} className='flex gap-1.5'>
-                              <img src={CheckIcon} alt="Check" />
-                                {feature}</li>
+                            return <li 
+                            key={index} 
+                            className='flex gap-1.5'>
+                              <img 
+                              src={CheckIcon} 
+                              alt="Check" />
+                            {feature}</li>
                         })}
                     </ul>
 
-                    <button className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-4 rounded-full font-bold text-xl text-white mr-7 hover:from-[#6351f8] hover:to-[#a83dfc] w-full'>Buy Now</button>
+                    <button 
+                    className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-4 rounded-full font-bold text-xl text-white mr-7 hover:from-[#6351f8] hover:to-[#a83dfc] w-full'>Buy Now</button>
 
                 </div>
             )}
